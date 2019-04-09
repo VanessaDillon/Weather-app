6dbf98d07743fc85123c310132c9ff9225d54443
Vanessa Dillon <vanessadillonmalisha@gmail.com>
2019-04-08 23:05:48 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
     // latitude: 32.350208, longitude: -89.99567359999999
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => { //find location
@@ -24,8 +26,22 @@ window.addEventListener('load', () => {
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
+                        //FORMULA FOR CELSIUS
+                        let celsius = (temperature - 32) * (5 / 9);
+
                     //Set Icon 
                     setIcons(icon, document.querySelector('.icon')); //class selector of icon in HTML to seticon
+
+                    //Change temperature to celsius
+                   temperatureSection.addEventListener('click', () => {
+                       if (temperatureSpan.textContent === "F"){
+                           temperatureSpan.textContent = "C";
+                           temperatureDegree.textContent = Math.floor(celsius);
+                       }else{
+                           temperatureSpan.textContent = "F";
+                           temperatureDegree.textContent = temperature;
+                       }
+                   })
                 });
             });
 
